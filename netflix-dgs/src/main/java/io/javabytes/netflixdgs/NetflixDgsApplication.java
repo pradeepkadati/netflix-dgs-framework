5cef5b1d6ab28603b8c768bfc5e1987c86fccc3e
pradeepkadati <pradeepkadati@gmail.com>
2023-12-28 21:59:36 -0800

    Netflix DGS Basics

---
+++ netflix-dgs/src/main/java/io/javabytes/netflixdgs/NetflixDgsApplication.java
@@ -0,0 +1,13 @@
+package io.javabytes.netflixdgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixDgsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixDgsApplication.class, args);
+    }
+
+}
